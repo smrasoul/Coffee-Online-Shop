8b28a69fa4b4157bf7cd5d4d7f01048b2d14118d
Rasoul <smrasoul@yahoo.com>
2023-10-18 19:43:44 +0330

    timer css added

--- main.js
+++ main.js
@@ -17,12 +17,14 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.querySelector('.timer-value-hours').innerHTML = hours;
+  document.querySelector('.timer-value-minutes').innerHTML = minutes;
+  document.querySelector('.timer-value-seconds').innerHTML = seconds;
+
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getAnimations("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
