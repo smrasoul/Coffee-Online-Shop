414f63f2f80a72110fe8d7a6d86f2d71d6271b29
Rasoul <smrasoul@yahoo.com>
2023-10-21 21:52:15 +0330

    Updated timer to show stacked hours

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("Oct 24, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -10,17 +10,23 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes, and seconds
+  // Both days/hours and stacked hours are included
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var hoursStacked = Math.floor((days * 24) + hours);
+
+  // Format hours, minutes, and seconds with at least 2 digits
+  var hoursFormatted = hoursStacked.toString().padStart(2, '0');
+  var minutesFormatted = minutes.toString().padStart(2, '0');
+  var secondsFormatted = seconds.toString().padStart(2, '0');
 
   // Display the result in the element with id="demo"
-  document.querySelector('.timer-value-hours').innerHTML = hours;
-  document.querySelector('.timer-value-minutes').innerHTML = minutes;
-  document.querySelector('.timer-value-seconds').innerHTML = seconds;
-
+  document.querySelector('.timer-value-hours').innerHTML = hoursFormatted;
+  document.querySelector('.timer-value-minutes').innerHTML = minutesFormatted;
+  document.querySelector('.timer-value-seconds').innerHTML = secondsFormatted;
 
   // If the count down is finished, write some text
   if (distance < 0) {
